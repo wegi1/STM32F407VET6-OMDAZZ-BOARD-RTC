814627977b14a1a444cf38bd1c77a01aef8b9d90
wegi <wegi01@outlook.com>
2026-01-11 14:54:33 +0100

    cls dayof week

--- USER/RTC.c
+++ USER/RTC.c
@@ -296,7 +296,8 @@ void print_RTC(void) {
 	lcd_mono_text_boxed(0, 125, tekst, digital_7_ttf, 40);
 
 
-	LCD_DisARectangular(200, 125, 319, 185, lcd_background_color); // clear for short name
+	LCD_DisARectangular(200, 125, 319, 188, lcd_background_color); // clear for short name
+	LCD_DisARectangular(88, 178, 200, 188, lcd_background_color); // clear BOTTOM OF LETTERS
 	lcd_text_boxed(88, 140, getDayofweek(my_date.WeekDay), dum1_ttf, 42);
 
 	hh = my_date.Year + ((century - 1) * 100);
